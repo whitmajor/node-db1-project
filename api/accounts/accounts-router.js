68c6e690a4b6b8e8dd03dd6d5f597c5f240c99a4
whitmajor <98010797+whitmajor@users.noreply.github.com>
2022-07-20 21:45:48 -0400

    end points working

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -2,26 +2,64 @@ const router = require('express').Router()
 
 router.get('/', (req, res, next) => {
   // DO YOUR MAGIC
+  try{
+    res.json("get account")
+
+  }catch(err){
+    next({ status:422, message: "this is horrible"})
+  }
 })
 
 router.get('/:id', (req, res, next) => {
   // DO YOUR MAGIC
+  try{
+    res.json("get account by id")
+
+  }catch(err){
+    next({ status:422, message: "this is horrible"})
+  }
 })
+
+
 
 router.post('/', (req, res, next) => {
   // DO YOUR MAGIC
+  try{
+    res.json("post account")
+
+  }catch(err){
+    next(err)
+  }
 })
+
 
 router.put('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
-});
+  try{
+    res.json("put account")
+
+  }catch(err){
+    next({ status:422, message: "this is horrible"})
+  }
+})
+
+
 
 router.delete('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+  try{
+    res.json("delete account")
+
+  }catch(err){
+    next({ status:422, message: "this is horrible"})
+  }
 })
 
+
 router.use((err, req, res, next) => { // eslint-disable-line
-  // DO YOUR MAGIC
+ res.status(err.status || 500).json({
+  message: err.message,
+ })
+
 })
+
 
 module.exports = router;
